4983f237ff4b81de9584b4f595a8cb47da1167c3
hiroe.eki <eki.hiroe@gmail.com>
2022-10-20 03:14:49 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,14 +10,18 @@
          <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li>{!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                    {{-- タスク作成ページへのリンク --}}
+                    <li>{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログアウトへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('logout.get', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
-            
         </div>
     </nav>
 </header>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,39 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>ユーザ登録はこちら</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        <h1>タスク一覧</h1>
+            @if (count($tasks) > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>タスク</th>
+                            <th>ステータス</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
+                         <tr>
+                            {{-- タスク詳細ページへのリンク --}}
+                            <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                            <td>{{ $task->content }}</td>
+                            <td>{{ $task->status }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+            
+            {{-- タスク作成ページへのリンク --}}
+            {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>ユーザ登録はこちら</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
-    
-    <h1>タスク一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                    <th>ステータス</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                 <tr>
-                    {{-- タスク詳細ページへのリンク --}}
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
-                    <td>{{ $task->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
     @endif
-    
-    {{-- タスク作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
-    
 @endsection
